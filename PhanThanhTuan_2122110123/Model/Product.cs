759ba54e8a8e99965c994e9f53d62b9b6e53d1c8
thanhtuandev <pro03tuan@gmail.com>
2025-03-29 08:24:07 +0700

    commit

---
+++ PhanThanhTuan_2122110123/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace PhanThanhTuan_2122110123.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Image { get; set; }
+    }
+}
